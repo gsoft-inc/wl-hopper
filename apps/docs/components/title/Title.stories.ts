86c685d185e578e09ee66f26d2522a4dfcb9074a
Franck <franck.gaudin@gsoft.com>
2023-04-18 16:16:27 -0400

    fix linting warning

--- apps/docs/components/title/Title.stories.ts
+++ apps/docs/components/title/Title.stories.ts
@@ -1,25 +1,25 @@
-import {Meta, StoryObj} from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/react";
 
-import { Title } from './Title';
+import { Title } from "./Title";
 
 const meta: Meta<typeof Title> = {
-  title: 'Component/Title',
-  component: Title,
-  argTypes: {
-    as: {
-      control: { type: 'select'}
-    },
-    level: {
-      control: { type: 'select'}
+    title: "Component/Title",
+    component: Title,
+    argTypes: {
+        as: {
+            control: { type: "select" }
+        },
+        level: {
+            control: { type: "select" }
+        }
     }
-  }
-}
+};
 
 export default meta;
 type Story = StoryObj<typeof Title>;
 
 export const Default: Story = {
-  args: {
-    children: 'Nothing is cheap'
-  }
-}
+    args: {
+        children: "Nothing is cheap"
+    }
+};
