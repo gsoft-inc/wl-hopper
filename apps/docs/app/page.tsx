e111895c4daa52f8ee997de9141176243e6b7bae
Franck <franck.gaudin@gsoft.com>
2023-04-19 15:49:59 -0400

    first steps of project

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -3,18 +3,7 @@ import { Title } from "@/components/title/Title";
 export default function Home() {
     return (
         <main>
-            <Title as="h1" level={1}>WL Documentation website</Title>
-            <Title as="h2" level={2}>WL Documentation website</Title>
-            <Title as="h3" level={3}>WL Documentation website</Title>
-            <Title as="h4" level={4}>WL Documentation website</Title>
-            <Title as="h5" level={5}>WL Documentation website</Title>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-              ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-              fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-              mollit anim id est laborum.
-            </p>
+            <Title as="h1" level={3}>The first steps of Untiteled project</Title>
         </main>
     );
 }
