f0df5b64419889c728946eebb23d50d04d9cf18a
Franck <franck.gaudin@gsoft.com>
2023-04-20 13:11:24 -0400

    fix a typo in the project name

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -3,7 +3,7 @@ import { Title } from "@/components/title/Title";
 export default function Home() {
     return (
         <main>
-            <Title as="h1" level={3}>The first steps of Untiteled project</Title>
+            <Title as="h1" level={3}>The first steps of Untitled project</Title>
         </main>
     );
 }
