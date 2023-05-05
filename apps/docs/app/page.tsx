f24b2eec5ecd8b8793d52852ae1afb17b6726eed
Francis Thibault <francis.thibault@gmail.com>
2023-05-05 13:42:19 -0400

    renamed untilted for hopper - added namespace for tokens - added a way to swap theme and store it

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -3,7 +3,7 @@ import { Title } from "@/components/Title/Title";
 export default function Home() {
     return (
         <main>
-            <Title as="h1" level={3}>The first steps of Untitled project</Title>
+            <Title as="h1" level={3}>The first  steps of Hopper</Title>
         </main>
     );
 }
--- apps/docs/components/IconButton/IconButton.tsx
+++ apps/docs/components/IconButton/IconButton.tsx
@@ -6,16 +6,72 @@ import { Icon } from "@/components/Icons/Icons";
 import "./iconButton.css";
 
 export const IconButton = () => {
-    const [ theme, setTheme ] = React.useState("light");
+    function hasLocalStoragePreference() {
+        if (typeof window !== "undefined" && window.localStorage) {
+            if (localStorage.getItem("hdTheme") !== null) {
+                return true;
+            }
+        }
+    }
+
+    function getTheme() {
+        if (hasLocalStoragePreference()) {
+            if (typeof window !== "undefined" && window.localStorage) {
+                return localStorage.getItem("hdTheme");
+            }
+        } else {
+            if (typeof window !== "undefined") {
+                return window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+            }
+        }
+    }
+
+    const [ theme, setTheme ] = React.useState(getTheme());
+
+    function setBodyDataAttribute() {
+        const rootElement = document.getElementById("App");
+
+        if (theme !== null && theme !== undefined) {
+            rootElement?.setAttribute("data-theme", theme);
+        }
+    }
+
+    function handlePress() {
+        if (theme === "light") {
+            setTheme("dark");
+            if (typeof window !== "undefined" && window.localStorage) {
+                localStorage.setItem("hdTheme", "dark");
+            }
+        } else if (theme === "dark") {
+            setTheme("light");
+            if (typeof window !== "undefined" && window.localStorage) {
+                localStorage.setItem("hdTheme", "light");
+            }
+        }
+    }
+
+    React.useEffect(() => {
+        if (theme !== null) {
+            setBodyDataAttribute();
+        }
+    }, [theme]);
+
+    if (typeof window !== "undefined") {
+        window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", e => {
+            if (!hasLocalStoragePreference()) {
+                setTheme(e.matches ? "dark" : "light");
+            }
+        });
+    }
 
     return (
         <Toggle.Root
             defaultPressed={theme === "light"}
-            onPressedChange={() => setTheme(theme === "light" ? "dark" : "light")}
+            onPressedChange={() => handlePress()}
             aria-label="Toggle dark/light mode"
             className="hd-iconButton"
         >
-            <Icon icon={ theme === "light" ? "moon" : "sun"} />
+            <Icon icon={ theme === "light" ? "moon" : "sun"}/>
         </Toggle.Root>
     );
 };
