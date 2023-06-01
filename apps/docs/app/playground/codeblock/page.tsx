375b44bd3b7397985ce6359dff5c559ffca94154
Francis Thibault <francis.thibault@gmail.com>
2023-06-01 15:08:50 -0400

    added a playground url for testing purposes

--- apps/docs/app/playground/codeblock/page.tsx
+++ apps/docs/app/playground/codeblock/page.tsx
@@ -3,12 +3,12 @@
 import { allPages } from "@/.contentlayer/generated";
 import { Mdx } from "@/components/Mdx/MdxComponent";
 
-import "../page.css";
+import "../../page.css";
 
 export default function Page() {
     return <main>
         {allPages
-            .filter(page => page._id === "pages/intro.mdx")
+            .filter(page => page._id === "pages/playground.mdx")
             .map(page => (
                 <article key={page._id}>
                     {page.body && <Mdx code={page.body.code} />}
