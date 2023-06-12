bb0ab71c5c3d339b40302a3b1f5933b07ea31baa
Francis Thibault <francis.thibault@gmail.com>
2023-06-12 08:22:47 -0400

    linting error fix

--- apps/docs/components/Sidebar/Sidebar.tsx
+++ apps/docs/components/Sidebar/Sidebar.tsx
@@ -22,7 +22,7 @@ export const Sidebar = () => {
                     <summary className="hd-sidebar-list__section-trigger hd-sidebar__list-item">
                         <span>Design Tokens</span>
                         <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" className="hd-sidebar-trigger__icon">
-                            <path d="M4 6L8 10L12 6" strokeWidth="1.33333" strokeLinecap="round" strokeLinejoin="round"/>
+                            <path d="M4 6L8 10L12 6" strokeWidth="1.33333" strokeLinecap="round" strokeLinejoin="round" />
                         </svg>
                     </summary>
                     <ul className="hd-sidebar__list hd-sidebar__list--collapsible" >
