e30bc2fb19a7bd224edc2e87705fa869fdddf91b
Francis Thibault <francis.thibault@gmail.com>
2023-07-11 10:47:02 -0400

    skeleton of Card Component

---
+++ apps/docs/components/Card/Card.tsx
@@ -0,0 +1,10 @@
+import type { ReactNode } from "react";
+// import "./card.css";
+
+export default function Card({ children }: { children: ReactNode }) {
+    return (
+        <div className="hd-card">
+            {children}
+        </div>
+    );
+}
