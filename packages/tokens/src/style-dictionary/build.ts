94b1dbb21b09c83d5d7c5353cfc2de43cdc8dc40
Alexandre Beaudoin <alexandre.beaudoin@gsoft.com>
2023-07-18 14:21:49 -0400

    Feat/storybook token display (#21)

--- packages/tokens/src/style-dictionary/build.ts
+++ packages/tokens/src/style-dictionary/build.ts
@@ -1,7 +1,19 @@
-import StyleDictionary from "style-dictionary";
+import StyleDictionary, { type TransformedTokens } from "style-dictionary";
 import config from "./config.ts";
 
 console.log("\nBuild started...");
+
+const handleGroups = (dataArray: TransformedTokens[]): string[] => {
+    if (!dataArray) {
+        return [];
+    }
+
+    const groups = dataArray.map(token => token.group);
+    const filteredGroups = groups.filter(group => group !== undefined);
+    const uniqueGroups = [...new Set(filteredGroups)];
+
+    return uniqueGroups.map(group => group.toString());
+};
 
 StyleDictionary.registerFormat({
     name: "css/dark-mode",
@@ -35,6 +47,43 @@ StyleDictionary.registerFormat({
         }).join("\n");
 
         return `:root {\n${lightTokens}\n}\n\n[data-theme="dark"] {\n${darkTokens}\n}`;
+    }
+});
+
+StyleDictionary.registerFormat({
+    name: "custom/doc",
+    formatter: function ({ dictionary }) {
+        const groups = handleGroups(dictionary.allTokens);
+
+        const dataArray = groups.map(group => {
+            const formattedGroup = group.charAt(0).toUpperCase() + group.slice(1);
+            let header = `\n  /**\n   * @tokens ${formattedGroup}s\n   * @presenter ${formattedGroup} \n   */\n\n`;
+            const data = dictionary.allTokens.filter(token => {
+                return token.group === group;
+            });
+
+            if (group === "mediaQuery" || group === "shadow" || group === "zIndex") {
+                header = `\n  /**\n   * @tokens ${formattedGroup} \n   */\n\n`;
+            }
+
+            return {
+                header,
+                data
+            };
+        });
+
+        const storybookDocs = dataArray
+            .map(function (tokens) {
+                return (
+                    tokens.header +
+                    tokens.data
+                        .map(props => `  --${props.name}: ${props.value};`)
+                        .join("\n")
+                );
+            })
+            .join("\n");
+
+        return `:root {${storybookDocs}\n}`;
     }
 });
 
