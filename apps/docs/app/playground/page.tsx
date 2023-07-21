8c5099b92d3f115662e5835999e431b66ef9039d
Franck <franck.gaudin@gsoft.com>
2023-07-21 13:12:47 -0400

    chore(doc): create an index page for the playground to facilitate navigation

---
+++ apps/docs/app/playground/page.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import Title from "@/components/ui/title/Title";
+
+export default function PlaygroundPage() {
+    return (
+        <main>
+            <Title level={2}>Playground Links</Title>
+            <ul style={{ listStyle: "none", display: "flex", flexDirection: "column", gap: "0.75rem", marginBlock: "2rem" }}>
+                <li>
+                    <Link href="/playground/codeblock">Codeblock →</Link>
+                </li>
+                <li>
+                    <Link href="/playground/headings-link">Heading link →</Link>
+                </li>
+            </ul>
+        </main>
+    );
+}
