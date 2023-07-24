0ab82e4a2e54c91038c71a86ca3ea327249e85f9
Franck <franck.gaudin@gsoft.com>
2023-07-24 14:42:14 -0400

    chore(doc): repalce note by componentContent

--- apps/docs/app/components/[slug]/page.tsx
+++ apps/docs/app/components/[slug]/page.tsx
@@ -9,13 +9,13 @@ interface PageProps {
 }
 
 async function getNoteFromParams(params: PageProps["params"]) {
-    const note = allComponents.find(component => component.slug === params.slug);
+    const componentContent = allComponents.find(component => component.slug === params.slug);
 
-    if (!note) {
+    if (!componentContent) {
         return null;
     }
 
-    return note;
+    return componentContent;
 }
 
 export async function generateStaticParams(): Promise<PageProps["params"][]> {
@@ -25,19 +25,19 @@ export async function generateStaticParams(): Promise<PageProps["params"][]> {
 }
 
 export default async function ComponentPage({ params }: PageProps) {
-    const note = await getNoteFromParams(params);
+    const componentContent = await getNoteFromParams(params);
 
-    if (!note) {
+    if (!componentContent) {
         notFound();
     }
 
     return (
         <main>
-            <article key={note._id}>
-                {note.status &&
-                    `status: ${note.status}`
+            <article key={componentContent._id}>
+                {componentContent.status &&
+                    `status: ${componentContent.status}`
                 }
-                {note.body && <Mdx code={note.body.code} />}
+                {componentContent.body && <Mdx code={componentContent.body.code} />}
             </article>
         </main>
     );
