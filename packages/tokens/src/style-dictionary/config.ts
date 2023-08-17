72b74a66f95bcb98e47a5dcf388cee8622aa9255
vicky-comeau <110498164+vicky-comeau@users.noreply.github.com>
2023-08-17 16:19:09 -0400

    Fix: Update readme and fonts config (#46)

--- packages/tokens/src/style-dictionary/config.ts
+++ packages/tokens/src/style-dictionary/config.ts
@@ -22,7 +22,7 @@ export const fontsConfig = {
                         }
                     },
                     "options": {
-                        "fontPathPrefix": "../"
+                        "fontPathPrefix": "./"
                     }
                 }
             ],
