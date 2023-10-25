7d92bdd3ec29c1c8a92efe0de1f4f7ac9f0b8d70
alexandre.asselin <alexandre.asselin@gsoft.com>
2023-10-25 12:28:19 -0400

    According to release notes, support for vite turbosnap has been added in chromatic 6.5

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,4 @@
 import type { StorybookConfig } from "@storybook/react-vite";
-import { mergeConfig } from "vite";
-import turbosnap from "vite-plugin-turbosnap";
 
 
 const storybookConfig: StorybookConfig = {
@@ -20,22 +18,6 @@ const storybookConfig: StorybookConfig = {
     },
     docs: {
         autodocs: "tag"
-    },
-    async viteFinal(config, { configType }) {
-        return mergeConfig(config, {
-            plugins:
-            configType === "PRODUCTION"
-                ? [
-                    // TurboSnap only officially support webpack (https://www.chromatic.com/docs/turbosnap/#prerequisites)
-                    // This plugin is suggested by storybook and maintained by a core storybook contributor.
-                    // This is experimental, and may not support all project and storybook configurations, yet.
-                    turbosnap({
-                    // This should be the base path of your storybook.  In monorepos, you may only need process.cwd().
-                        rootDir: config.root ?? process.cwd()
-                    })
-                ]
-                : []
-        });
     }
 };
 
