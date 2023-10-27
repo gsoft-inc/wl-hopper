439119025a945abbf086f793ef33a8988dcb5608
alexandre.asselin <alexandre.asselin@gsoft.com>
2023-10-27 17:05:00 -0400

    fix comment

--- packages/styled-system/src/tokens/useInjectTokens.tsx
+++ packages/styled-system/src/tokens/useInjectTokens.tsx
@@ -4,15 +4,15 @@ import { Tokens } from "./tokens.ts";
 /**
  * Injects the tokens into the DOM as CSS variables, under the rootSelector className.
  * @example
- * assuming different versions of the design system loaded at the same time, one with version 1, the other with version 1.1.0
+ * assuming different versions of the design system loaded at the same time, one with version 1, the other with version 2.0.0
  *  <head>
  *      <style id="hop-tokens-1.0.0">
  *          .hop-1-0-0 {
  *              --token: red
  *          }
  *       </style>
- *       <style id="hop-tokens-1.1.0">
- *          .hop-1-1-0 {
+ *       <style id="hop-tokens-2.0.0">
+ *          .hop-2-0-0 {
  *              --token: blue
  *          }
  *       </style>
