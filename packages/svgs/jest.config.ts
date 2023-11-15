d06d80df98960309328b08cb167af9afabbb7ec3
alexandre.asselin <alexandre.asselin@gsoft.com>
2023-11-15 14:25:43 -0500

    tentative jest fix

--- packages/svgs/jest.config.ts
+++ packages/svgs/jest.config.ts
@@ -1,25 +1,12 @@
 import type { Config } from "jest";
 import { swcConfig } from "./swc.jest.ts";
-import path from "path";
 
 const config: Config = {
-    testEnvironment: "jsdom",
+    testEnvironment: "node",
     transform: {
         "^.+\\.(js|ts|tsx)$": ["@swc/jest", swcConfig as Record<string, unknown>]
     },
-    transformIgnorePatterns: [
-        // Must exclude hast-util-select from the transform ignore files
-        // because it's an ESM only project which must be processed by SWC.
-        // The pattern is optimized for PNPM, for more info view: https://jestjs.io/docs/configuration#transformignorepatterns-arraystring.
-        `${path.join(
-            __dirname,
-            "../.."
-        )}/node_modules/.pnpm/(?!(hast-util-select)@)`,
-        `${path.join(
-            __dirname,
-            "../.."
-        )}/node_modules/.pnpm/(?!(property-information)@)`
-    ]
+    extensionsToTreatAsEsm: [".ts"]
 };
 
 export default config;
