de9aee9446bf0b134b3acceb4ad64dd723942a84
alexandre.asselin <alexandre.asselin@gsoft.com>
2023-11-20 11:32:21 -0500

    first draft of the svgr generation

--- packages/icons/scripts/constants.ts
+++ packages/icons/scripts/constants.ts
@@ -1,2 +1,5 @@
 export const SVGsDirectory = "node_modules/@hopper-ui/svgs/dist/icons";
 export const ComponentDirectory = "src/icons";
+
+export const NeutralIconColor = "#3C3C3C"; // --hop-neutral-icon
+export const PrimaryIconColor = "#3B57FF"; // --hop-primary-icon
--- packages/icons/scripts/generate-components.ts
+++ packages/icons/scripts/generate-components.ts
@@ -1,4 +1,5 @@
-
+import { transform } from "@svgr/core";
+import { PrimaryIconColor } from "./constants.ts";
 
 export interface SVGData {
     data: string;
@@ -6,6 +7,46 @@ export interface SVGData {
     size: number;
 }
 
-export async function generateComponents(SVGsDir: string, svgData: SVGData[]) {
-    console.log("SVG Data: ", svgData);
+export async function generateComponents(SVGsDir: string, svgDatas: SVGData[]) {
+    const fromKebabToPascalCase = (str: string) => {
+        return str.split("-").map(s => s.charAt(0).toUpperCase() + s.slice(1)).join("");
+    };
+
+    // console.log("SVG Data: ", svgDatas);
+    const test = [svgDatas[0], svgDatas[1]];
+    for (const svgData of test) {
+        const data = transform.sync(svgData.data, {
+            typescript: true,
+            ref: true,
+            replaceAttrValues: {
+                [PrimaryIconColor]: "var(--coucou)" // TODO fix this
+            },
+            jsxRuntime: "automatic",
+            svgProps: {
+                "focusable": "false"
+            },
+            // svgoConfig: config
+            // add svgo config to remove xmlns attribute
+            plugins: ["@svgr/plugin-jsx"],
+            template: ({ componentName, jsx, props, imports }, { tpl }) => {
+                return tpl`
+                ${componentName.endsWith("16") ? imports : ""}
+                ${componentName.endsWith("16") ? "/* eslint-disable max-len */" : ""}
+
+                const ${componentName} = forwardRef((${props}) => (
+                    ${jsx}
+                ));
+                `;
+            }
+        }, {
+            componentName: fromKebabToPascalCase(svgData.name)
+        });
+        // add this to the package.json
+        /*
+        "@svgr/plugin-svgo": "8.1.0", // optinonal?
+    "@svgr/plugin-jsx": "8.1.0"
+"@svgr/core": "8.1.0",
+        */
+        console.log(data);
+    }
 }
