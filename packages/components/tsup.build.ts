f49a6c57e30a197e633e6787853dcef1ebc43b40
alexandre.asselin <alexandre.asselin@gsoft.com>
2023-12-19 09:19:00 -0500

    Change build target to target es2019. Some tools (storybook) do not support ES2020 synthax. I had a super hard time changing the tool config to accept this, so instead i'm compiling hopper in an older version

--- packages/components/tsup.build.ts
+++ packages/components/tsup.build.ts
@@ -1,5 +1,6 @@
 import { defineBuildConfig } from "@workleap/tsup-configs";
 
 export default defineBuildConfig({
-    entry: ["./src/**/src/*.(ts|tsx)"]
+    entry: ["./src/**/src/*.(ts|tsx)"],
+    target: "es2019" // We set target ES2019 since ES2020 syntax is not supported by older versions of storybook (used in orbiter)
 });
