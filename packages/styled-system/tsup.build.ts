7f259c1a535ad76a09955b2fea9f0bfd788db346
Alexandre Asselin <alexasselin008@gmail.com>
2023-12-19 09:32:40 -0500

    Change build target to target es2019.  (#119)

--- packages/styled-system/tsup.build.ts
+++ packages/styled-system/tsup.build.ts
@@ -4,6 +4,7 @@ import packageJson from "./package.json";
 
 export default defineBuildConfig({
     entry: ["./src/**/*.(ts|tsx)"],
+    target: "es2019", // We set target ES2019 since ES2020 syntax is not supported by older versions of storybook (used in orbiter)
     esbuildPlugins: [
         createCssModuleEsbuildPlugin({
             generateScopedName: "[name]__[local]___[hash:base64:5]",
