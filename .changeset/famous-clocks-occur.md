20515b921e70eb0ce7c9696bf05fc93ffb2dc9f2
Alexandre Asselin <alexasselin008@gmail.com>
2024-01-12 13:13:47 -0500

    patch: Change the breakpoint context to now throw when the provider has not been declared (#126)

---
+++ .changeset/famous-clocks-occur.md
@@ -0,0 +1,5 @@
+---
+"@hopper-ui/styled-system": patch
+---
+
+Modify the breakpoint context to not throw, so components can render even if no provider has been declared
--- packages/styled-system/src/responsive/BreakpointContext.tsx
+++ packages/styled-system/src/responsive/BreakpointContext.tsx
@@ -1,20 +1,17 @@
 import { createContext, useContext } from "react";
-import { isNil } from "../utils/assertion.ts";
 import type { Breakpoint } from "./Breakpoints.ts";
 
 export interface BreakpointContextType {
     matchedBreakpoints: Breakpoint[];
 }
 
-export const BreakpointContext = createContext<BreakpointContextType | undefined>(undefined);
+export const BreakpointContext = createContext<BreakpointContextType>({
+    matchedBreakpoints: []
+});
 
 
 export function useBreakpointContext() {
     const context = useContext(BreakpointContext);
-
-    if (isNil(context)) {
-        throw new Error("useBreakpointContext must be used within a BreakpointProvider");
-    }
 
     return context;
 }
