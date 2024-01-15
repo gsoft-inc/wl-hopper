4219afc3c9f3c81a0632f34747f1c4fae84c1c00
Alexandre Asselin <alexandre.asselin@gsoft.com>
2024-01-15 16:48:20 -0500

    Update generated icons

--- packages/icons-react16/src/generated-icon-components/PlaySolidIcon.tsx
+++ packages/icons-react16/src/generated-icon-components/PlaySolidIcon.tsx
@@ -5,8 +5,8 @@
 import { createIcon } from "../create-icon.tsx";
 import React, { forwardRef, type Ref, type SVGProps } from "react";
 
-const PlaySolidIcon16 = forwardRef((props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => <svg width={16} height={16} fill="none" viewBox="0 0 16 16" ref={ref} {...props}><path fill="currentColor" fillRule="evenodd" d="m5.5 14.062 9-5.196a1 1 0 0 0 0-1.732l-9-5.196a1 1 0 0 0-1.5.866v10.392a1 1 0 0 0 1.5.866" /></svg>);
-const PlaySolidIcon24 = forwardRef((props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => <svg width={24} height={24} fill="none" viewBox="0 0 24 24" ref={ref} {...props}><path fill="currentColor" fillRule="evenodd" d="M21.25 13.299c1-.577 1-2.02 0-2.598L8.5 3.34a1.5 1.5 0 0 0-2.25 1.299V19.36a1.5 1.5 0 0 0 2.25 1.3z" /></svg>);
-const PlaySolidIcon32 = forwardRef((props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => <svg width={32} height={32} fill="none" viewBox="0 0 32 32" ref={ref} {...props}><path fill="currentColor" fillRule="evenodd" d="M11.5 27.258 28 17.732c1.333-.77 1.333-2.694 0-3.464L11.5 4.742c-1.333-.77-3 .192-3 1.732v19.052c0 1.54 1.667 2.502 3 1.732" /></svg>);
+const PlaySolidIcon16 = forwardRef((props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => <svg width={16} height={16} viewBox="0 0 16 16" ref={ref} {...props}><path fillRule="evenodd" d="m5.5 14.062 9-5.196a1 1 0 0 0 0-1.732l-9-5.196a1 1 0 0 0-1.5.866v10.392a1 1 0 0 0 1.5.866" /></svg>);
+const PlaySolidIcon24 = forwardRef((props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => <svg width={24} height={24} viewBox="0 0 24 24" ref={ref} {...props}><path fillRule="evenodd" d="M21.25 13.299c1-.577 1-2.02 0-2.598L8.5 3.34a1.5 1.5 0 0 0-2.25 1.299V19.36a1.5 1.5 0 0 0 2.25 1.3z" /></svg>);
+const PlaySolidIcon32 = forwardRef((props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => <svg width={32} height={32} viewBox="0 0 32 32" ref={ref} {...props}><path fillRule="evenodd" d="M11.5 27.258 28 17.732c1.333-.77 1.333-2.694 0-3.464L11.5 4.742c-1.333-.77-3 .192-3 1.732v19.052c0 1.54 1.667 2.502 3 1.732" /></svg>);
 
 export const PlaySolidIcon = createIcon(PlaySolidIcon16, PlaySolidIcon24, PlaySolidIcon32, "PlaySolidIcon");
--- packages/icons/src/generated-icon-components/PlayIcon.tsx
+++ packages/icons/src/generated-icon-components/PlayIcon.tsx
@@ -5,8 +5,8 @@
 import { createIcon } from "../create-icon.tsx";
 import { forwardRef, type Ref, type SVGProps } from "react";
 
-const PlayIcon16 = forwardRef((props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => <svg width={16} height={16} fill="none" viewBox="0 0 16 16" ref={ref} {...props}><path fill="currentColor" fillRule="evenodd" d="M13.025 8.012 4.5 2.874V13.15l8.525-5.139m1.49.854a.993.993 0 0 0 0-1.707L4.543 1.15C3.867.74 3 1.22 3 2.001v12.021c0 .781.867 1.26 1.543.854l9.972-6.011" /></svg>);
-const PlayIcon24 = forwardRef((props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => <svg width={24} height={24} fill="none" viewBox="0 0 24 24" ref={ref} {...props}><path fill="currentColor" fillRule="evenodd" d="M20.25 10.701c1 .577 1 2.02 0 2.598L7.5 20.66a1.5 1.5 0 0 1-2.25-1.299V4.64a1.5 1.5 0 0 1 2.25-1.3zm-13.5 8.66L19.5 12 6.75 4.639V19.36" /></svg>);
-const PlayIcon32 = forwardRef((props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => <svg width={32} height={32} fill="none" viewBox="0 0 32 32" ref={ref} {...props}><path fill="currentColor" fillRule="evenodd" d="M27.5 14.268c1.333.77 1.333 2.694 0 3.464l-17.25 9.96c-1.333.77-3-.193-3-1.733V6.041c0-1.54 1.667-2.502 3-1.732zM26.5 16 9.25 6.04v19.92z" /></svg>);
+const PlayIcon16 = forwardRef((props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => <svg width={16} height={16} viewBox="0 0 16 16" ref={ref} {...props}><path fillRule="evenodd" d="M13.025 8.012 4.5 2.874V13.15l8.525-5.139m1.49.854a.993.993 0 0 0 0-1.707L4.543 1.15C3.867.74 3 1.22 3 2.001v12.021c0 .781.867 1.26 1.543.854l9.972-6.011" /></svg>);
+const PlayIcon24 = forwardRef((props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => <svg width={24} height={24} viewBox="0 0 24 24" ref={ref} {...props}><path fillRule="evenodd" d="M20.25 10.701c1 .577 1 2.02 0 2.598L7.5 20.66a1.5 1.5 0 0 1-2.25-1.299V4.64a1.5 1.5 0 0 1 2.25-1.3zm-13.5 8.66L19.5 12 6.75 4.639V19.36" /></svg>);
+const PlayIcon32 = forwardRef((props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => <svg width={32} height={32} viewBox="0 0 32 32" ref={ref} {...props}><path fillRule="evenodd" d="M27.5 14.268c1.333.77 1.333 2.694 0 3.464l-17.25 9.96c-1.333.77-3-.193-3-1.733V6.041c0-1.54 1.667-2.502 3-1.732zM26.5 16 9.25 6.04v19.92z" /></svg>);
 
 export const PlayIcon = createIcon(PlayIcon16, PlayIcon24, PlayIcon32, "PlayIcon");
