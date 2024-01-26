45d52f10f4ccd1b24394a22e2b01506793f058bf
Alexandre Asselin <alexandre.asselin@gsoft.com>
2024-01-26 15:56:25 -0500

    update packages and remove icon-react16

--- packages/icons-react16/scripts/constants.ts
+++
@@ -1,6 +0,0 @@
-export const ComponentDirectory = "src/generated-icon-components";
-export const SVGsDirectory = "../svg-icons/src/optimized-icons/";
-export const IconSizes = [16, 24, 32] as const;
-
-export const NeutralIconColor = "#3C3C3C"; // --hop-neutral-icon
-export const PrimaryIconColor = "#3B57FF"; // --hop-primary-icon
--- packages/icons-react16/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./Icon.tsx";
-export * from "./create-icon.tsx";
-export * from "./generated-icon-components/index.ts";
-
