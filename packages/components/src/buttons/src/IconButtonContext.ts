11c3c0c53133e321ea868dfcd19a326ce80f9d6f
Alexandre Asselin <alexandre.asselin@gsoft.com>
2024-03-05 17:10:17 -0500

    remove icon buttons

--- packages/components/src/buttons/src/IconButtonContext.ts
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-import type { ContextValue } from "react-aria-components";
-import type { IconButtonProps } from "./IconButton.tsx";
-
-export const IconButtonContext = createContext<ContextValue<IconButtonProps, HTMLButtonElement>>({});
-
-IconButtonContext.displayName = "IconButtonContext";
--- packages/components/src/buttons/src/index.ts
+++ packages/components/src/buttons/src/index.ts
@@ -1,4 +1,2 @@
 export * from "./Button.tsx";
-export * from "./IconButton.tsx";
 export * from "./ButtonContext.ts";
-export * from "./IconButtonContext.ts";
