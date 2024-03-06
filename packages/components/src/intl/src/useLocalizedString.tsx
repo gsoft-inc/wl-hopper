80214f851e2509807286a0249eddabaf4ba31f2c
Alexandre Asselin <alexandre.asselin@gsoft.com>
2024-03-06 11:35:29 -0500

    fix build

--- packages/components/src/intl/src/useLocalizedString.tsx
+++ packages/components/src/intl/src/useLocalizedString.tsx
@@ -8,6 +8,22 @@ const Resources = {
     "fr-CA": resourcesFrCa
 };
 
-export function useLocalizedString() {
+export function useLocalizedString(): LocalizedStringFormatter {
     return useLocalizedStringFormatter(Resources, "@hopper-ui/components");
+}
+
+/**
+ * LocalizedStringFormatter is not exported from react-aria, so we have to define it here.
+ * Without this, the build will fail with the following error:
+* packages/components build:
+ * src/intl/src/useLocalizedString.tsx(11,17): error TS2742:
+ * The inferred type of 'useLocalizedString' cannot be named without a reference to
+ * .pnpm/@internationalized+string@3.2.1/node_modules/@internationalized/string'.
+ * This is likely not portable. A type annotation is necessary.
+ *
+ * I  proposed a PR to export LocalizedStringFormatter from react-aria.
+ * https://github.com/adobe/react-spectrum/pull/6014
+ */
+interface LocalizedStringFormatter {
+    format: (key: keyof typeof Resources[keyof typeof Resources], variables?: Record<string, string | number | boolean>) => string;
 }
