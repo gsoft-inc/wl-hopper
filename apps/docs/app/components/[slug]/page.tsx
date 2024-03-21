1f65c0b2b6ff91ea8b32dece82a4fb6161dfedeb
Franck Gaudin <franck.gaudin@workleap.com>
2024-03-21 09:24:32 -0400

    feat: update component page

--- apps/docs/app/components/[slug]/page.tsx
+++ apps/docs/app/components/[slug]/page.tsx
@@ -1,6 +1,13 @@
 import { notFound } from "next/navigation";
 import { allComponents } from "contentlayer/generated";
+
+import Tag from "@/components/ui/tag/Tag";
+import LinkList from "@/components/linkList/LinkList";
+
 import { getComponentDetails } from "@/utils/getComponentDetails.ts";
+import Title from "@/components/ui/title/Title.tsx";
+
+import "./components.css";
 
 interface PageProps {
     params: {
@@ -23,48 +30,72 @@ export default async function ComponentPage({ params }: PageProps) {
 
     const { content, frontmatter: { title, status, description, links } } = component;
 
+    const linksList = [
+        {
+            name: "github",
+            src: links.source,
+            label: "View source"
+        },
+        {
+            name: "npm",
+            src: links.npm,
+            label: "View on npm"
+        },
+        {
+            name: "issue",
+            src: links.issue,
+            label: "Report an issue"
+        }
+    ];
+
     return (
         <div className="hd-container">
             <main>
-                <h1>{title}</h1><span>{status}</span>
+                <div className="hd-flex hd-component-heading">
+                    <Title as="h1">{title}</Title>
+                    {status && <Tag>{status}</Tag>}
+                </div>
+
                 <p>{description}</p>
-                {links.source && <a href={links.source}>Github</a>}
+                
+                <LinkList links={linksList} />
+
                 {content}
 
                 {/*the sections below are simply for zoning purposes, they will be replaced by real content*/}
-                <section>
-                    <h2>Usage</h2>
-                    <p>Alerts are used to communicate a state that affects the entire system, not just a single
-                        component. They are used to communicate a state that affects the entire system, not just a
-                        single component. They are used to communicate a state that affects the entire system, not just
-                        a single component. They are used to communicate a state that affects the entire system, not
-                        just a single component.</p>
-                </section>
+                {/*<section>*/}
+                {/*    <h2>Usage</h2>*/}
+                {/*    <p>Alerts are used to communicate a state that affects the entire system, not just a single*/}
+                {/*        component. They are used to communicate a state that affects the entire system, not just a*/}
+                {/*        single component. They are used to communicate a state that affects the entire system, not just*/}
+                {/*        a single component. They are used to communicate a state that affects the entire system, not*/}
+                {/*        just a single component.</p>*/}
+                {/*</section>*/}
 
-                <section>
-                    <h2>Anatomy</h2>
-                </section>
+                {/*<section>*/}
+                {/*    <h2>Anatomy</h2>*/}
+                {/*</section>*/}
 
-                <section>
-                    <h2>API</h2>
-                </section>
+                {/*<section>*/}
+                {/*    <h2>API</h2>*/}
+                {/*</section>*/}
 
-                <section>
-                    <h2>Examples</h2>
-                </section>
+                {/*<section>*/}
+                {/*    <h2>Examples</h2>*/}
+                {/*</section>*/}
 
-                <section>
-                    <h2>Accessibility</h2>
-                </section>
+                {/*<section>*/}
+                {/*    <h2>Accessibility</h2>*/}
+                {/*</section>*/}
 
-                <section>
-                    <h2>Related</h2>
-                </section>
+                {/*<section>*/}
+                {/*    <h2>Related</h2>*/}
+                {/*</section>*/}
 
-                <div>
-                    <button type="submit">Preview</button>
-                    <button type="submit">Next</button>
-                </div>
+                {/*<div>*/}
+                {/*    <button type="submit">Preview</button>*/}
+                {/*    <button type="submit">Next</button>*/}
+                {/*</div>*/}
             </main>
         </div>
     );
