d2fe7b7910d1c6e3298c245ddf327d636e19e39f
Francis Thibault <francis.thibault@gsoft.com>
2024-03-25 11:11:39 -0400

    modified the header to fit the new design

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -2,8 +2,7 @@ export default function Home() {
     return (
         <div className="hd-wrapper hd-flex">
             <main className="hd-home">
-                <h1 className="hd-display">Welcome to Workleap's <strong className="hd-text--strong">Hopper</strong> Design System.</h1>
-                <p className="hd-display__subtitle">The documentation is currently in <strong className="hd-text--strong">beta</strong>.</p>
+                Home Alone
             </main>
         </div>
     );
