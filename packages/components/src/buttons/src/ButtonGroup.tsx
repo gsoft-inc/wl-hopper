58cac0fbf44764e0345d31e1da1b96a242dc584f
Alexandre Asselin <alexandre.asselin@gsoft.com>
2024-04-03 09:57:24 -0400

    review fixes

--- packages/components/src/buttons/src/ButtonGroup.tsx
+++ packages/components/src/buttons/src/ButtonGroup.tsx
@@ -1,21 +1,3 @@
-//   /** Whether the Buttons in the ButtonGroup are all disabled. */
-//   isDisabled?: boolean,
-//   /**
-//    * The axis the ButtonGroup should align with. Setting this to 'vertical' will prevent
-//    * any switching behaviors between 'vertical' and 'horizontal'.
-//    * @default 'horizontal'
-//    */
-//   orientation?: Orientation,
-//   /** The Buttons contained within the ButtonGroup. */
-//   children: ReactNode,
-//   /**
-//    * The alignment of the buttons within the ButtonGroup.
-//    * @default 'start'
-//    */
-//   align?: Alignment | 'center'
-// size
-// fluid
-
 import { forwardRef, type ForwardedRef, type HTMLAttributes, type CSSProperties } from "react";
 import { ButtonContext } from "./ButtonContext.ts";
 import type { Orientation } from "react-aria";
