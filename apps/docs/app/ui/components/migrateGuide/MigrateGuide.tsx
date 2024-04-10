fb598e16772f9c21a739ccee221a9d25ac4b3eac
Franck Gaudin <franck.gaudin@workleap.com>
2024-04-10 10:32:27 -0400

    Component migration section added

--- apps/docs/app/ui/components/migrateGuide/MigrateGuide.tsx
+++ apps/docs/app/ui/components/migrateGuide/MigrateGuide.tsx
@@ -5,9 +5,13 @@ export interface MigrateGuideProps {
 }
 
 const MigrateGuide = async ({ src }: MigrateGuideProps) => {
+    if (!src) {
+        return null;
+    }
+
     const { content } = await getMigrationNotes(src);
 
-    return { content };
+    return <article>{content}</article>;
 };
 
 export default MigrateGuide;
