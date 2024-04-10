6dcf2c9c4f72351cb205e705174ad2c0d7910fd2
Alexandre Asselin <alexandre.asselin@gsoft.com>
2024-04-10 15:03:22 -0400

    update scripts and tests for rich icons in svg-icons

--- packages/svg-icons/scripts/constants.ts
+++ packages/svg-icons/scripts/constants.ts
@@ -1,6 +1,19 @@
-export const IconsSourceDirectory = "src/icons";
-export const IconsDistDirectory = "src/optimized-icons";
-export const IconSizes = [16, 24, 32] as const;
+
+// Those are hardcoded since they are used directly in the svg files.
 export const NeutralIconColor = "#3C3C3C"; // --hop-neutral-icon
 export const PrimaryIconColor = "#3B57FF"; // --hop-primary-icon
 export const WarningWeakIconColor = "#E57723"; // --hop-warning-icon-weak
+
+export const DecorativeOption7IconColor = "#2A2620"; // --hop-decorative-option7-icon
+export const DecorativeOption7SurfaceColor = "#E5DED6"; // --hop-decorative-option7-surface
+export const NeutralSurface = "white"; // --hop-status-neutral-surface
+
+export const IconsSourceDirectory = "src/icons";
+export const IconsDistDirectory = "src/optimized-icons";
+export const IconSizes = [16, 24, 32] as const;
+export const AllowedIconFillColors = [NeutralIconColor, PrimaryIconColor, WarningWeakIconColor];
+
+export const RichIconsSourceDirectory = "src/rich-icons";
+export const RichIconsDistDirectory = "src/optimized-rich-icons";
+export const RichIconSizes = [24, 32, 40] as const;
+export const RichAllowedIconFillColors = [DecorativeOption7IconColor, DecorativeOption7SurfaceColor, NeutralSurface];
