8111f6d6d9608381b690e4cde97b62adeea27eb5
vicky-comeau <vicky.comeau@gsoft.com>
2024-04-19 14:46:42 -0400

    fixed import order

--- packages/components/setupTests.ts
+++ packages/components/setupTests.ts
@@ -1,12 +1,12 @@
 import "@testing-library/jest-dom";
 import "@testing-library/jest-dom/jest-globals";
-import { enableFetchMocks } from "jest-fetch-mock";
 import failOnConsole from "jest-fail-on-console";
+import { enableFetchMocks } from "jest-fetch-mock";
 
 // This is used for tests with react-router-dom.
 // There was an error thrown "ReferenceError: Request is not defined"
 enableFetchMocks();
 
 // This will fail the test if there is a console.error or console.warn
-failOnConsole()
+failOnConsole();
 
