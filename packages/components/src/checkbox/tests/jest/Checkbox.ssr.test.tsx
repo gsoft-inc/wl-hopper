aabbc9d91a8ccc46f577641f04a1196ae9142269
Alexandre Asselin <alexandre.asselin@gsoft.com>
2024-04-19 14:42:52 -0400

    Add missing SSR tests

--- packages/components/src/checkbox/tests/jest/Checkbox.ssr.test.tsx
+++ packages/components/src/checkbox/tests/jest/Checkbox.ssr.test.tsx
@@ -4,12 +4,21 @@
 import { renderToString } from "react-dom/server";
 
 import { Checkbox } from "../../src/Checkbox.tsx";
+import { CheckboxGroup } from "../../src/CheckboxGroup.tsx";
+import { CheckboxField } from "../../src/CheckboxField.tsx";
+import { Text } from "../../../Text/index.ts";
 
 describe("Checkbox", () => {
     it("should render on the server", () => {
         const renderOnServer = () =>
             renderToString(
-                <Checkbox>Text</Checkbox>
+                <CheckboxGroup aria-label="options">
+                    <Checkbox>option 1</Checkbox>
+                    <CheckboxField>
+                        <Checkbox>option 1</Checkbox>
+                        <Text slot="description">description</Text>
+                    </CheckboxField>
+                </CheckboxGroup>
             );
 
         expect(renderOnServer).not.toThrow();
--- packages/components/src/radio/tests/jest/Radio.ssr.test.tsx
+++ packages/components/src/radio/tests/jest/Radio.ssr.test.tsx
@@ -5,12 +5,20 @@ import { renderToString } from "react-dom/server";
 
 import { Radio } from "../../src/Radio.tsx";
 import { RadioGroup } from "../../src/RadioGroup.tsx";
+import { RadioField } from "../../src/RadioField.tsx";
+import { Text } from "../../../Text/index.ts";
 
 describe("Radio", () => {
     it("should render on the server", () => {
         const renderOnServer = () =>
             renderToString(
-                <RadioGroup aria-label="options"><Radio value="text">Text</Radio></RadioGroup>
+                <RadioGroup aria-label="options">
+                    <Radio value="text">Text</Radio>
+                    <RadioField>
+                        <Radio value="option1">option 1</Radio>
+                        <Text slot="description">description</Text>
+                    </RadioField>
+                </RadioGroup>
             );
 
         expect(renderOnServer).not.toThrow();
