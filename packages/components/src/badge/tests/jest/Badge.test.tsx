4e66f23f4a550880cb39cce1500e80772329612a
vicky-comeau <vicky.comeau@gsoft.com>
2024-05-21 14:42:34 -0400

    Fixed tests

--- packages/components/src/badge/tests/jest/Badge.test.tsx
+++ packages/components/src/badge/tests/jest/Badge.test.tsx
@@ -6,58 +6,58 @@ import { BadgeContext } from "../../src/BadgeContext.ts";
 
 describe("Badge", () => {
     it("should render with default class", () => {
-        render(<Badge>12</Badge>);
+        render(<Badge data-testid="badge">12</Badge>);
 
-        const element = screen.getByText("12");
+        const element = screen.getByTestId("badge");
         expect(element).toHaveClass("hop-Badge");
     });
 
     it("should support custom class", () => {
-        render(<Badge className="test">12</Badge>);
+        render(<Badge data-testid="badge" className="test">12</Badge>);
 
-        const element = screen.getByText("12");
+        const element = screen.getByTestId("badge");
         expect(element).toHaveClass("hop-Badge");
         expect(element).toHaveClass("test");
     });
 
     it("should support custom style", () => {
-        render(<Badge marginTop="stack-sm" style={{ marginBottom: "13px" }} >12</Badge>);
+        render(<Badge data-testid="badge" marginTop="stack-sm" style={{ marginBottom: "13px" }} >12</Badge>);
 
-        const element = screen.getByText("12");
+        const element = screen.getByTestId("badge");
         expect(element).toHaveStyle({ marginTop: "var(--hop-space-stack-sm)", marginBottom: "13px" });
     });
 
     it("should support DOM props", () => {
-        render(<Badge data-foo="bar">12</Badge>);
+        render(<Badge data-testid="badge" data-foo="bar">12</Badge>);
 
-        const element = screen.getByText("12");
+        const element = screen.getByTestId("badge");
         expect(element).toHaveAttribute("data-foo", "bar");
     });
 
     it("should support slots", () => {
         render(
             <BadgeContext.Provider value={{ slots: { test: { "aria-label": "test" } } }}>
-                <Badge slot="test">12</Badge>
+                <Badge data-testid="badge" slot="test">12</Badge>
             </BadgeContext.Provider>
         );
 
-        const element = screen.getByText("12");
+        const element = screen.getByTestId("badge");
         expect(element).toHaveAttribute("slot", "test");
         expect(element).toHaveAttribute("aria-label", "test");
     });
 
     it("should support refs", () => {
         const ref = createRef<HTMLSpanElement>();
-        render(<Badge ref={ref}>12</Badge>);
+        render(<Badge data-testid="badge" ref={ref}>12</Badge>);
 
         expect(ref.current).not.toBeNull();
         expect(ref.current instanceof HTMLSpanElement).toBeTruthy();
     });
 
     it("should add proper attributes when disabled", () => {
-        render(<Badge isDisabled>12</Badge>);
+        render(<Badge data-testid="badge" isDisabled>12</Badge>);
 
-        const element = screen.getByText("12");
+        const element = screen.getByTestId("badge");
         expect(element).toHaveAttribute("data-disabled");
         expect(element).toHaveAttribute("aria-disabled");
     });
--- packages/components/src/overlineText/tests/jest/OverlineText.test.tsx
+++ packages/components/src/overlineText/tests/jest/OverlineText.test.tsx
@@ -7,62 +7,51 @@ import { OverlineTextContext } from "../../src/OverlineTextContext.ts";
 
 describe("OverlineText", () => {
     it("should render with default class", () => {
-        render(<OverlineText>Test</OverlineText>);
+        render(<OverlineText data-testid="overline-text">Test</OverlineText>);
 
-        const element = screen.getByText("Test");
+        const element = screen.getByTestId("overline-text");
         expect(element).toHaveClass("hop-OverlineText");
     });
 
     it("should support custom class", () => {
-        render(<OverlineText className="test">Test</OverlineText>);
+        render(<OverlineText data-testid="overline-text" className="test">Test</OverlineText>);
 
-        const element = screen.getByText("Test");
+        const element = screen.getByTestId("overline-text");
         expect(element).toHaveClass("hop-OverlineText");
         expect(element).toHaveClass("test");
     });
 
     it("should support custom style", () => {
-        render(<OverlineText marginTop="stack-sm" style={{ marginBottom: "13px" }} >Test</OverlineText>);
+        render(<OverlineText data-testid="overline-text" marginTop="stack-sm" style={{ marginBottom: "13px" }} >Test</OverlineText>);
 
-        const element = screen.getByText("Test");
+        const element = screen.getByTestId("overline-text");
         expect(element).toHaveStyle({ marginTop: "var(--hop-space-stack-sm)", marginBottom: "13px" });
     });
 
     it("should support DOM props", () => {
-        render(<OverlineText data-foo="bar">Test</OverlineText>);
+        render(<OverlineText data-testid="overline-text" data-foo="bar">Test</OverlineText>);
 
-        const element = screen.getByText("Test");
+        const element = screen.getByTestId("overline-text");
         expect(element).toHaveAttribute("data-foo", "bar");
     });
 
     it("should support slots", () => {
         render(
             <OverlineTextContext.Provider value={{ slots: { test: { "aria-label": "test" } } }}>
-                <OverlineText slot="test">Test</OverlineText>
+                <OverlineText data-testid="overline-text" slot="test">Test</OverlineText>
             </OverlineTextContext.Provider>
         );
 
-        const element = screen.getByText("Test");
+        const element = screen.getByTestId("overline-text");
         expect(element).toHaveAttribute("slot", "test");
         expect(element).toHaveAttribute("aria-label", "test");
     });
 
     it("should support refs", () => {
         const ref = createRef<HTMLSpanElement>();
-        render(<OverlineText ref={ref}>Test</OverlineText>);
+        render(<OverlineText data-testid="overline-text" ref={ref}>Test</OverlineText>);
 
         expect(ref.current).not.toBeNull();
         expect(ref.current instanceof HTMLSpanElement).toBeTruthy();
-    });
-
-    it("should stop context propagation on nested text", () => {
-        render(
-            <ButtonGroupContext.Provider value={{ className: "testClass" }}>
-                <OverlineText>Test <OverlineText>Nested</OverlineText></OverlineText>
-            </ButtonGroupContext.Provider>
-        );
-
-        const element = screen.getByText("Nested");
-        expect(element).not.toHaveClass("testClass");
     });
 });
