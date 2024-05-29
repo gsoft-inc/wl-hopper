c701eb5aabb4c604180ed934cfc16f0daac9af85
Franck Gaudin <franck.gaudin@workleap.com>
2024-05-29 12:36:30 -0400

    chore: add new export

--- packages/components/src/buttons/src/index.ts
+++ packages/components/src/buttons/src/index.ts
@@ -1,4 +1,6 @@
 export * from "./Button.tsx";
 export * from "./ButtonContext.ts";
+export * from "./ButtonGroup.tsx";
+export * from "./ButtonGroupContext.ts";
 export * from "./ClearButton.tsx";
 export * from "./ClearButtonContext.ts";
--- packages/components/src/layout/index.ts
+++ packages/components/src/layout/index.ts
@@ -2,3 +2,5 @@ export * from "./src/Flex.tsx";
 export * from "./src/Grid.tsx";
 export * from "./src/Inline.tsx";
 export * from "./src/Stack.tsx";
+export * from "./src/Footer.tsx";
+export * from "./src/FooterContext.ts";
