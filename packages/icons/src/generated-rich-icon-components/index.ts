e94a3e088048e68b2d8855254ae9f0f3221bf8fe
Francis Thibault <francis.thibault@gsoft.com>
2024-06-10 21:24:58 -0400

    added new Rich Icons

--- packages/icons/src/generated-rich-icon-components/index.ts
+++ packages/icons/src/generated-rich-icon-components/index.ts
@@ -5,19 +5,27 @@
 /* eslint-disable */
 
 export * from "./ActionListRichIcon.tsx";
+export * from "./AnniversaryRichIcon.tsx";
 export * from "./AnonymousRichIcon.tsx";
+export * from "./BirthdayRichIcon.tsx";
+export * from "./BrokenImageRichIcon.tsx";
 export * from "./CautionRichIcon.tsx";
 export * from "./ConversationRichIcon.tsx";
 export * from "./CustomRichIcon.tsx";
 export * from "./DecreasingScoreRichIcon.tsx";
+export * from "./DepartmentRichIcon.tsx";
+export * from "./DirectoryRichIcon.tsx";
+export * from "./DiscountRichIcon.tsx";
 export * from "./EmailRichIcon.tsx";
 export * from "./FeedbackRichIcon.tsx";
 export * from "./GoalIndividualRichIcon.tsx";
 export * from "./GoalTeamRichIcon.tsx";
+export * from "./GroupRichIcon.tsx";
 export * from "./GrowthRichIcon.tsx";
 export * from "./IdeaRichIcon.tsx";
 export * from "./InfoRichIcon.tsx";
 export * from "./LikertScaleRichIcon.tsx";
+export * from "./LocationRichIcon.tsx";
 export * from "./MultipleChoiceRichIcon.tsx";
 export * from "./OneOnOneRichIcon.tsx";
 export * from "./OpinionScaleRichIcon.tsx";
@@ -32,10 +40,12 @@ export * from "./ScriptRichIcon.tsx";
 export * from "./SettingsRichIcon.tsx";
 export * from "./SparklesRichIcon.tsx";
 export * from "./StarRichIcon.tsx";
+export * from "./StatusRichIcon.tsx";
 export * from "./SuccessRichIcon.tsx";
 export * from "./SupportRichIcon.tsx";
 export * from "./TemplateRichIcon.tsx";
 export * from "./TextAnswerRichIcon.tsx";
 export * from "./UpsellRichIcon.tsx";
+export * from "./WhosWhoRichIcon.tsx";
 
-export const richIconNames = ["ActionListRichIcon","AnonymousRichIcon","CautionRichIcon","ConversationRichIcon","CustomRichIcon","DecreasingScoreRichIcon","EmailRichIcon","FeedbackRichIcon","GoalIndividualRichIcon","GoalTeamRichIcon","GrowthRichIcon","IdeaRichIcon","InfoRichIcon","LikertScaleRichIcon","MultipleChoiceRichIcon","OneOnOneRichIcon","OpinionScaleRichIcon","OrganizationRichIcon","PeopleRichIcon","QuestionRichIcon","ReminderRichIcon","ReviewRichIcon","RisingScoreRichIcon","RocketRichIcon","ScriptRichIcon","SettingsRichIcon","SparklesRichIcon","StarRichIcon","SuccessRichIcon","SupportRichIcon","TemplateRichIcon","TextAnswerRichIcon","UpsellRichIcon"] as const;
+export const richIconNames = ["ActionListRichIcon","AnniversaryRichIcon","AnonymousRichIcon","BirthdayRichIcon","BrokenImageRichIcon","CautionRichIcon","ConversationRichIcon","CustomRichIcon","DecreasingScoreRichIcon","DepartmentRichIcon","DirectoryRichIcon","DiscountRichIcon","EmailRichIcon","FeedbackRichIcon","GoalIndividualRichIcon","GoalTeamRichIcon","GroupRichIcon","GrowthRichIcon","IdeaRichIcon","InfoRichIcon","LikertScaleRichIcon","LocationRichIcon","MultipleChoiceRichIcon","OneOnOneRichIcon","OpinionScaleRichIcon","OrganizationRichIcon","PeopleRichIcon","QuestionRichIcon","ReminderRichIcon","ReviewRichIcon","RisingScoreRichIcon","RocketRichIcon","ScriptRichIcon","SettingsRichIcon","SparklesRichIcon","StarRichIcon","StatusRichIcon","SuccessRichIcon","SupportRichIcon","TemplateRichIcon","TextAnswerRichIcon","UpsellRichIcon","WhosWhoRichIcon"] as const;
