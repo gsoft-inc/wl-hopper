f983c22bd042ecbad196f627222cc3091c8bba12
Franck Gaudin <franck.gaudin@workleap.com>
2024-06-17 12:32:17 -0400

    chore(doc): fix lint error

--- apps/docs/app/ui/components/componentExample/ComponentPreview.stories.tsx
+++ apps/docs/app/ui/components/componentExample/ComponentPreview.stories.tsx
@@ -12,7 +12,6 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
     args: {
-        src: "buttons/docs/preview",
-        colorScheme: "light"
+        src: "buttons/docs/preview"
     }
 };
