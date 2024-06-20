a2cc69ff8e560a8066ef5b0b7b0d77610fb3713a
Francis Thibault <francis.thibault@gsoft.com>
2024-06-20 14:50:36 -0400

    pr fiixes

--- apps/docs/app/ui/components/overview/Overview.tsx
+++ apps/docs/app/ui/components/overview/Overview.tsx
@@ -1,7 +1,8 @@
-"use client";
-
 import { allComponents } from "@/.contentlayer/generated";
+import Link from "next/link";
 import Image from "next/image";
+import Title from "@/components/title/Title";
+
 import "./overview.css";
 
 const Overview = () => {
@@ -12,18 +13,18 @@ const Overview = () => {
         <div className="hd-component-overview-wrapper">
             {categories.map(category => (
                 <div className="hd-component-overview-category" key={category}>
-                    <h2 className="hd-component-overview-category__title">{category}</h2>
+                    <Title as="h3" level="h2" className="hd-component-overview-category__title">{category}</Title>
                     <div className="hd-component-overview">
                         {allComponents.filter(component => component.category && component.category === category).map(component => (
-                            <a key={component._id} className="hd-component-overview__item" href={`/${component._raw.flattenedPath}`}>
+                            <Link key={component._id} className="hd-component-overview__item" href={`/${component._raw.flattenedPath}`}>
                                 <div className="hd-component-overview__item-thumb">
                                     <Image src={`https://place-hold.it/260x140?text=${component.title}&fontsize=20`} alt="thumb" width="260" height="140" className="hd-component-overview__item-img" />
                                 </div>
                                 <div className="hd-component-overview-item__caption">
-                                    <h3>{component.title}</h3>
+                                    <Title as="h3" level="h3">{component.title}</Title>
                                     <p className="hd-component-overview__item-teaser">{component.description}</p>
                                 </div>
-                            </a>
+                            </Link>
                         ))}
                     </div>
                 </div>
