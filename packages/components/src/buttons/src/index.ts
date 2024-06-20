ddcb8694d62b4e5aae2f0ff92315752eb7b90200
Franck Gaudin <franck.gaudin@workleap.com>
2024-06-20 10:05:31 -0400

    chore(doc): remove tests

--- packages/components/src/buttons/src/index.ts
+++ packages/components/src/buttons/src/index.ts
@@ -1,6 +1,4 @@
-import { slot } from "@hopper-ui/styled-system";
-
-// export * from "./Button.tsx";
+export * from "./Button.tsx";
 export * from "./ButtonContext.ts";
 export * from "./ButtonGroup.tsx";
 export * from "./ButtonGroupContext.ts";
@@ -8,8 +6,3 @@ export * from "./ClearButton.tsx";
 export * from "./ClearButtonContext.ts";
 export * from "./EmbeddedButton.tsx";
 export * from "./EmbeddedButtonContext.ts";
-
-import { Button } from "./Button.tsx";
-
-const _Button = slot("button", Button);
-export { _Button as Button };
