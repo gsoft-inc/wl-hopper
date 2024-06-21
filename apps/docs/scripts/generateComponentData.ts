f32d63fa1410a0f621cb8d870419b181a27ce034
Alexandre Asselin <alexandre.asselin@gsoft.com>
2024-06-21 14:30:25 -0400

    fix linting

--- apps/docs/scripts/generateComponentData.ts
+++ apps/docs/scripts/generateComponentData.ts
@@ -176,7 +176,7 @@ async function generateComponentData() {
     };
 
     const components = await generateComponentList(PACKAGES, options);
-    console.log('Found components:', components)
+    console.log("Found components:", components);
 
     if (!components.length) {
         console.error("No components found");
