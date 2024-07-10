7f5d8c59d81bfc2244345639dda92737cfb2eea6
Franck Gaudin <franck.gaudin@workleap.com>
2024-07-10 15:21:00 -0400

    chore(docs): update content with Pull request comments

--- apps/docs/examples/Preview.ts
+++ apps/docs/examples/Preview.ts
@@ -80,30 +80,6 @@ export const Previews: Record<string, Preview> = {
     "HopperProvider/docs/responsive-styles/useResponsiveValue": {
         component: lazy(() => import("@/../../packages/components/src/HopperProvider/docs/responsive-styles/useResponsiveValue.tsx"))
     },
-    "layout/docs/flex/preview": {
-        component: lazy(() => import("@/../../packages/components/src/layout/docs/flex/preview.tsx"))
-    },
-    "layout/docs/flex/verticalAlignment": {
-        component: lazy(() => import("@/../../packages/components/src/layout/docs/flex/verticalAlignment.tsx"))
-    },
-    "layout/docs/flex/gap": {
-        component: lazy(() => import("@/../../packages/components/src/layout/docs/flex/gap.tsx"))
-    },
-    "layout/docs/flex/alignItems": {
-        component: lazy(() => import("@/../../packages/components/src/layout/docs/flex/alignItems.tsx"))
-    },
-    "layout/docs/flex/justifyContent": {
-        component: lazy(() => import("@/../../packages/components/src/layout/docs/flex/justifyContent.tsx"))
-    },
-    "layout/docs/flex/reverse": {
-        component: lazy(() => import("@/../../packages/components/src/layout/docs/flex/reverse.tsx"))
-    },
-    "layout/docs/flex/wrap": {
-        component: lazy(() => import("@/../../packages/components/src/layout/docs/flex/wrap.tsx"))
-    },
-    "layout/docs/flex/nesting": {
-        component: lazy(() => import("@/../../packages/components/src/layout/docs/flex/nesting.tsx"))
-    },
     "avatar/docs/preview": {
         component: lazy(() => import("@/../../packages/components/src/avatar/docs/preview.tsx"))
     },
@@ -140,4 +116,29 @@ export const Previews: Record<string, Preview> = {
     "avatar/docs/customization": {
         component: lazy(() => import("@/../../packages/components/src/avatar/docs/customization.tsx"))
     },
+    "layout/docs/flex/preview": {
+        component: lazy(() => import("@/../../packages/components/src/layout/docs/flex/preview.tsx"))
+    },
+    "layout/docs/flex/verticalAlignment": {
+        component: lazy(() => import("@/../../packages/components/src/layout/docs/flex/verticalAlignment.tsx"))
+    },
+    "layout/docs/flex/gap": {
+        component: lazy(() => import("@/../../packages/components/src/layout/docs/flex/gap.tsx"))
+    },
+    "layout/docs/flex/alignItems": {
+        component: lazy(() => import("@/../../packages/components/src/layout/docs/flex/alignItems.tsx"))
+    },
+    "layout/docs/flex/justifyContent": {
+        component: lazy(() => import("@/../../packages/components/src/layout/docs/flex/justifyContent.tsx"))
+    },
+    "layout/docs/flex/reverse": {
+        component: lazy(() => import("@/../../packages/components/src/layout/docs/flex/reverse.tsx"))
+    },
+    "layout/docs/flex/wrap": {
+        component: lazy(() => import("@/../../packages/components/src/layout/docs/flex/wrap.tsx"))
+    },
+    "layout/docs/flex/nesting": {
+        component: lazy(() => import("@/../../packages/components/src/layout/docs/flex/nesting.tsx"))
+    },
 };
+    
