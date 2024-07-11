5a742945b7f570746f638101551c3deec9e0de74
Franck Gaudin <franck.gaudin@workleap.com>
2024-07-11 15:16:31 -0400

    chore(docs): update content with Pull request comments

--- packages/components/src/Link/docs/inheritSize.tsx
+++ packages/components/src/Link/docs/inheritSize.tsx
@@ -3,7 +3,7 @@ import { Link, Text } from "@hopper-ui/components";
 export default function Example() {
     return (
         <Text size="lg">
-            Would you like to <Link href="#">learn more</Link> about this fine component?
+            Would you like to <Link href="#">learn more</Link> about this role?
         </Text>
     );
 }
--- packages/components/src/Link/docs/quiet.tsx
+++ packages/components/src/Link/docs/quiet.tsx
@@ -3,7 +3,7 @@ import { Link } from "@hopper-ui/components";
 export default function Example() {
     return (
         <p>
-            Would you like to <Link isQuiet href="#">learn more</Link> about this fine component?
+            Would you like to <Link isQuiet href="#">learn more</Link> about this role?
         </p>
     );
 }
