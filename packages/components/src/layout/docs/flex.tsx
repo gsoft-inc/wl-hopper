e2a32920ee83ee70c09a308f9fb35e74ca6a0cf0
Francis Thibault <francis.thibault@gsoft.com>
2024-07-17 12:55:10 -0400

    removed test that was left there by mistake

--- packages/components/src/layout/docs/flex.tsx
+++ packages/components/src/layout/docs/flex.tsx
@@ -2,7 +2,7 @@ import { Div, Flex } from "@hopper-ui/components";
 
 export default function Example() {
     return (
-        <Flex direction="column" width="core_1280" gap="inline-sm" backgroundColorActive="primary-disabled">
+        <Flex direction="column" width="core_1280" gap="inline-sm">
             <Div backgroundColor="decorative-option1" height="core_640" />
             <Div backgroundColor="decorative-option3" height="core_640" />
             <Div backgroundColor="decorative-option4" height="core_640" />
