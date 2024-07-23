dd06d87c2bb4739f8888a6d20e617e3fcdd632fa
Francis Thibault <francis.thibault@gsoft.com>
2024-07-23 17:19:58 -0400

    wip

--- apps/docs/app/lib/getSectionLinks.ts
+++ apps/docs/app/lib/getSectionLinks.ts
@@ -5,21 +5,30 @@ type SectionLink = Pick<MDX, "raw">;
 
 function getSectionLinks(content: {
     body: SectionLink;
+    lastLevel2Title: string;
 }) {
     const regex = /(^#{2,3}\s).*?(?=\n)/gm;
     const body = content.body.raw;
     const matches = body.match(regex);
+    let lastLevel2Title = "";
 
     if (matches) {
         return matches.map(link => {
-            console.log(link);
-
             const title = link.replace("###", "").replace("##", "").trim();
             const level = link.startsWith("###") ? 3 : 2;
 
+            if (level === 2) {
+                lastLevel2Title = title;
+            }
+
+            const url = level === 3 && lastLevel2Title
+                ? `#${formattingTitleId(lastLevel2Title.toString())}-${formattingTitleId(title.toString())}`
+                : `#${formattingTitleId(title.toString())}`;
+
+
             return {
                 title: title,
-                url: `#${formattingTitleId(title.toString())}`,
+                url: url,
                 id: title.toLowerCase().replace(/\s+/g, "-"),
                 level: level
             };
