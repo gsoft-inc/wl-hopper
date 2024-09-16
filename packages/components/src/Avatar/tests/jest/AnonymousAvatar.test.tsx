558013ece972286e0ca1b40ae842fcb5063e3371
Victor Trinh <victor.trinh@gsoft.com>
2024-09-16 10:18:33 -0400

    AnonymousAvatar

--- packages/components/src/Avatar/tests/jest/AnonymousAvatar.test.tsx
+++ packages/components/src/Avatar/tests/jest/AnonymousAvatar.test.tsx
@@ -58,10 +58,10 @@ describe("AnonymousAvatar", () => {
         render(
             <AnonymousAvatar aria-label="Maye Musk" />
         );
-    
+
         expect(await screen.findByLabelText("Maye Musk")).not.toBeNull();
     });
-    
+
     it("should support refs", () => {
         const ref = createRef<HTMLDivElement>();
         render(<AnonymousAvatar aria-label="John Doe" ref={ref} />);
@@ -72,12 +72,17 @@ describe("AnonymousAvatar", () => {
         expect(ref.current instanceof HTMLDivElement).toBeTruthy();
         expect(ref.current?.tagName.toUpperCase()).toBe("DIV");
     });
-    
-    it("should add data-disabled when isDisabled is true", () => {
-        render(<AnonymousAvatar aria-label="John Doe" isDisabled />);
+
+    it("should support disabled state", () => {
+        render(<AnonymousAvatar
+            aria-label="John Doe"
+            isDisabled
+            className={({ isDisabled }) => (isDisabled ? "disabled" : "")}
+        />);
 
         const element = screen.getByRole("img", { name: "John Doe" });
 
         expect(element).toHaveAttribute("data-disabled");
+        expect(element).toHaveClass("disabled");
     });
 });
