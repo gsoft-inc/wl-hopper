492edce35e29c420ec45a9936ce6d804699325f9
Francis Thibault <francis.thibault@gsoft.com>
2024-10-09 11:36:29 -0400

    updated the PR to use children prop from rac disclosure

--- apps/docs/components/collapsible/Collapsible.tsx
+++ apps/docs/components/collapsible/Collapsible.tsx
@@ -3,27 +3,34 @@
 import { CollapseIcon, Icon } from "@/components/icon";
 import clsx from "clsx";
 import type { ReactNode } from "react";
-import { Button, UNSTABLE_Disclosure as Disclosure, UNSTABLE_DisclosurePanel as DisclosurePanel, type DisclosureProps } from "react-aria-components";
+import { Button, composeRenderProps, UNSTABLE_Disclosure as Disclosure, UNSTABLE_DisclosurePanel as DisclosurePanel, type DisclosureProps } from "react-aria-components";
 
 import "./collapsible.css";
 
 export interface CollapsibleProps extends DisclosureProps {
-    children: ReactNode;
     title: ReactNode;
 }
 
-const Collapsible = ({ title, className, children, ...rest }: CollapsibleProps) => {
+const Collapsible = ({ title, className, children: childrenProp, ...rest }: CollapsibleProps) => {
+    const children = composeRenderProps(childrenProp, prev => {
+        return prev ;
+    });
+
     return (
         <Disclosure className={clsx("hd-collapsible", className)} {...rest}>
-            <h3>
-                <Button className="hd-collapsible__trigger" slot="trigger">
-                    {title}
-                    <Icon src={CollapseIcon} />
-                </Button>
-            </h3>
-            <DisclosurePanel>
-                <div className="hd-collapsible__content">{children}</div>
-            </DisclosurePanel>
+            {disclosureRenderProps => (
+                <>
+                    <h3>
+                        <Button className="hd-collapsible__trigger" slot="trigger">
+                            {title}
+                            <Icon src={CollapseIcon} />
+                        </Button>
+                    </h3>
+                    <DisclosurePanel>
+                        <div className="hd-collapsible__content">{children(disclosureRenderProps)}</div>
+                    </DisclosurePanel>
+                </>
+            )}
         </Disclosure>
     );
 };
