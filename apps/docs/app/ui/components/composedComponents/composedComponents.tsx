2b54ba33bca951659a7b77f3e3eec420c6d92314
Francis Thibault <francis.thibault@gsoft.com>
2024-10-17 09:49:27 -0400

    addef a failsafe alphabetical sorting

--- apps/docs/app/ui/components/composedComponents/composedComponents.tsx
+++ apps/docs/app/ui/components/composedComponents/composedComponents.tsx
@@ -15,10 +15,12 @@ const ComposedComponents = ({ components } : ComposedComponentsProps) => {
     const { colorMode } = useContext(ThemeContext);
     const theme = colorMode as ColorScheme;
 
+    const sortedComponents = [...components].sort((a, b) => a.localeCompare(b));
+
     return (
         <HopperProvider colorScheme={theme}>
             <div className="hd-composed-components__wrapper">
-                {components.map(component => (
+                {sortedComponents.map(component => (
                     <OverviewTile title={component} key={component} />
                 ))}
             </div>
