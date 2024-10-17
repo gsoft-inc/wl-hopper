030d2079c563a7952812c7325d334a137db75d72
vicky-comeau <vicky.comeau@gsoft.com>
2024-10-17 14:32:07 -0400

    Removed RadioField that was not needed

--- packages/components/src/radio/docs/radiogroup/description.tsx
+++ packages/components/src/radio/docs/radiogroup/description.tsx
@@ -1,13 +1,11 @@
-import { Radio, RadioField, RadioGroup } from "@hopper-ui/components";
+import { Radio, RadioGroup } from "@hopper-ui/components";
 
 export default function Example() {
     return (
         <RadioGroup aria-label="roles" description="Select one to continue">
             <Radio value="developer">Developer</Radio>
             <Radio value="designer">Designer</Radio>
-            <RadioField>
-                <Radio value="manager">Manager</Radio>
-            </RadioField>
+            <Radio value="manager">Manager</Radio>
         </RadioGroup>
     );
 }
