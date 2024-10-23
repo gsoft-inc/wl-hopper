b79fdbf9e43682e6d8a836a8f19809e543123689
Alexandre Asselin <alexandre.asselin@gsoft.com>
2024-10-23 16:02:09 -0400

    add sample app to hopper

---
+++ apps/samples/basic/src/Layout.tsx
@@ -0,0 +1,51 @@
+import { Button, Div, H1, HopperProvider, HtmlHeader, Link, Main, useColorSchemeContext } from "@hopper-ui/components";
+import { Outlet, useHref, useNavigate, type NavigateOptions } from "react-router-dom";
+
+declare module "react-aria-components" {
+    interface RouterConfig {
+        routerOptions: NavigateOptions;
+    }
+}
+
+export function Layout() {
+    const navigate = useNavigate();
+
+    return (
+        <HopperProvider locale="en-US" navigate={navigate} useHref={useHref} withBodyStyle defaultColorScheme="dark">
+            <InnerLayout />
+        </HopperProvider>
+    );
+}
+
+function InnerLayout() {
+    const { setColorScheme, colorScheme } = useColorSchemeContext();
+
+    return (
+        <Div>
+            <HtmlHeader borderBottom="neutral" padding="inset-sm">
+                <H1>React App</H1>
+                <Link href="/">Main Page</Link> &nbsp;
+                <Link href="/store">Store</Link>
+                <Div
+                    position="absolute"
+                    top="10px"
+                    right="10px"
+                    display="fixed"
+                    UNSAFE_width="200px"
+                >
+
+                    <Button
+                        size="sm"
+                        variant="secondary"
+                        onPress={() => {
+                            setColorScheme(colorScheme === "light" ? "dark" : "light");
+                        }}
+                    >Change Theme</Button>
+                </Div>
+            </HtmlHeader>
+            <Main padding="inset-lg">
+                <Outlet />
+            </Main>
+        </Div>
+    );
+}
---
+++ apps/samples/basic/src/index.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { App } from "./App.tsx";
+import "./index.css";
+
+const root = createRoot(document.getElementById("root")!);
+
+root.render(
+    <StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </StrictMode>
+);
