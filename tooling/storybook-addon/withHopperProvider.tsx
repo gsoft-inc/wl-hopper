7cda1eb891c9fc7d5cd62e56a5c763ab1bebcc03
Alexandre Asselin <alexandre.asselin@gsoft.com>
2024-12-09 11:17:10 -0500

    fix normal ci

--- tooling/storybook-addon/withHopperProvider.tsx
+++ tooling/storybook-addon/withHopperProvider.tsx
@@ -1,6 +1,7 @@
 import { type ColorScheme, HopperProvider } from "@hopper-ui/components";
 import { makeDecorator } from "@storybook/preview-api";
-import { JSX } from "react";
+import type { JSX } from "react";
+
 import { DisableAnimations } from "./DisableAnimations.tsx";
 import "./disableAnimations.css";
 
