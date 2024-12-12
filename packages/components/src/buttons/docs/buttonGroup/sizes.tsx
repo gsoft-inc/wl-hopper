64075fa9376133555ff1742a60ee7830622441de
Alexandre Asselin <alexandre.asselin@gsoft.com>
2024-12-12 13:00:49 -0500

    fix small mistake

--- packages/components/src/buttons/docs/buttonGroup/sizes.tsx
+++ packages/components/src/buttons/docs/buttonGroup/sizes.tsx
@@ -4,13 +4,13 @@ export default function Example() {
     return (
         <Stack>
             <ButtonGroup>
-                <Button variant="secondary">No, thanks</Button>,
-                <Button variant="secondary">Remind me later</Button>,
+                <Button variant="secondary">No, thanks</Button>
+                <Button variant="secondary">Remind me later</Button>
                 <Button variant="primary">Rate Now</Button>
             </ButtonGroup>
             <ButtonGroup size="sm">
-                <Button variant="secondary">No, thanks</Button>,
-                <Button variant="secondary">Remind me later</Button>,
+                <Button variant="secondary">No, thanks</Button>
+                <Button variant="secondary">Remind me later</Button>
                 <Button variant="primary">Rate Now</Button>
             </ButtonGroup>
         </Stack>
