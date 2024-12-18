3f02f06b01bbb2f8bd64d03bc271b8354cdf8ca5
Victor Trinh <victor.trinh@gsoft.com>
2024-12-18 16:09:58 -0500

    Remove title slot

--- packages/components/src/overlays/Popover/docs/controlled.tsx
+++ packages/components/src/overlays/Popover/docs/controlled.tsx
@@ -17,7 +17,7 @@ export default function Example() {
                 <span ref={triggerRef}>Popover will be positioned relative to me</span>
             </Stack>
             <Popover triggerRef={triggerRef} isOpen={isOpen} onOpenChange={setOpen}>
-                <Heading slot="title">ACME</Heading>
+                <Heading>ACME</Heading>
                 <Content>
                     A tech company focusing on the development of software and hardware solutions.
                 </Content>
