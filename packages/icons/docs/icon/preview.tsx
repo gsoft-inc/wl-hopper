61d8eed3840076d605fb39a9e94514ad1dea0ac5
Victor Trinh <victor.trinh@gsoft.com>
2024-12-26 12:22:02 -0500

    Rich Icon documentation

--- packages/icons/docs/icon/preview.tsx
+++ packages/icons/docs/icon/preview.tsx
@@ -1,11 +1,11 @@
 import { createIcon } from "@hopper-ui/icons";
 
-import { Sparkles16, Sparkles24, Sparkles32 } from "./src/index.ts";
+import { Sparkles16, Sparkles24, Sparkles32 } from "../src/index.ts";
 
 const CustomIcon = createIcon(Sparkles16, Sparkles24, Sparkles32, "CustomIcon");
 
 export default function Example() {
     return (
-        <CustomIcon fill="primary" />
+        <CustomIcon />
     );
 }
--- packages/icons/docs/src/index.ts
+++ packages/icons/docs/src/index.ts
@@ -1,3 +1,7 @@
 export { Sparkles16 } from "./Sparkles16.tsx";
 export { Sparkles24 } from "./Sparkles24.tsx";
 export { Sparkles32 } from "./Sparkles32.tsx";
+export { SparklesRichIcon24 } from "./SparklesRich24.tsx";
+export { SparklesRichIcon32 } from "./SparklesRich32.tsx";
+export { SparklesRichIcon40 } from "./SparklesRich40.tsx";
+
